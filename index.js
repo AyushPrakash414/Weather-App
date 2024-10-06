5188199c8256ae9d42cdd132c36686406ba5029c
Ayush <prakashayush414@gmail.com>
2024-10-06 19:00:39 +0530

    Adding Forcasting Information in Input bar

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ windspeed=document.querySelector("#WindSpeed")
 day1=document.querySelector("#day1")
 desc=document.querySelector("#Description")
 let ayush=fetch(`https://goweather.herokuapp.com/weather/${data.value}`)
-
+day1=document.querySelector("#day1");
+day2=document.querySelector("#day2");
+day3=document.querySelector("#day3");
 ayush
 .then((value1)=>{
     return value1.json();
@@ -20,6 +22,9 @@ ayush
     windspeed.value=value2.wind;
     desc.value=value2.description;
     console.log(value2);
+    day1.value=value2.forecast[0].temperature;
+    day2.value=value2.forecast[1].temperature;
+    day3.value=value2.forecast[2].temperature;
 })
 .catch((error) => {
     console.log(error);
